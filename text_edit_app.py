2d6b6195f17faafab81fdc43da7ef92ffa65c6aa
Tyler <tylerdevon01@gmail.com>
2020-02-09 23:05:54 -0500

    Base

--- text_edit_app.py
+++ text_edit_app.py
@@ -1,5 +1,6 @@
 from tkinter import filedialog
 from tkinter import *
+from tkinter import ttk
 import ntpath
 
 class Window(Frame):
@@ -16,8 +17,12 @@ class Window(Frame):
         self.sidebar.pack(expand=True, fill='both', side='left', anchor='nw')
 
         # Mainarea
-        self.mainarea = Frame(master, bg='#CCC', width=500, height=500)
+        self.mainarea = Frame(master, bg='white', width=500, height=500)
         self.mainarea.pack(expand=True, fill='both', side='right')
+
+        # Tabs
+        self.tabs = Frame(self.mainarea, width=500, height=2, bg='white')
+        self.tabs.pack(side=TOP, anchor='nw')
 
         # Scrollbar
         self.scrollbar = Scrollbar(self.mainarea)
@@ -31,6 +36,49 @@ class Window(Frame):
         self.scrollbar.config(command=self.text_area.yview)
 
         self.filename = None
+        self.current_file = None
+
+    def open_file(self):
+        self.filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("py files","*.py"), ("txt files", "*.txt"), ("all files","*.*")))
+        if not self.filename: return
+
+        try:
+            text1 = open(self.filename).read()
+            self.text_area.insert(END, text1)
+
+            # Add filename to sidebar
+            self.add_file_to_sidebar(self.filename)
+            self.add_file_to_tab(self.filename)
+        except Exception:
+            print('Error')
+
+    def save_file(self):
+        if self.filename == None:
+            # If no filename in state, which means new file.
+            f = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+            if f is None:
+                return
+            text2save = str(self.text_area.get(1.0, END))
+            f.write(text2save)
+            f.close()
+        else:
+            # If filename in state, which means opened file
+            f = open(self.filename, "r+")
+            my_txt = str(self.text_area.get(1.0, END))
+            f.writelines(my_txt)
+            f.close()
+
+    def add_file_to_sidebar(self, name):
+        fname = pretty_filename(name)
+        label = Label(self.sidebar, text=fname, width=15, height=1, anchor=NW, justify=LEFT)
+        label.pack(fill=X)
+        
+        label.bind("<Button-1>", self.showFile)
+    
+    def add_file_to_tab(self, name):
+        fname = pretty_filename(name)
+        label = Label(self.tabs, text=fname, width=15, height=1, anchor=NW, justify=LEFT, bg='white')
+        label.pack()
 
     def init_menu(self):
         menubar = Menu(self.master)
@@ -53,41 +101,10 @@ class Window(Frame):
     def my_command(self):
         print('hello world')
 
-    def open_file(self):
-        self.filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("py files","*.py"), ("txt files", "*.txt"), ("all files","*.*")))
-        if not self.filename: return
-
-        try:
-            text1 = open(self.filename).read()
-            self.text_area.insert(END, text1)
-
-            # Add filename to sidebar
-            fname = pretty_filename(self.filename)
-            self.add_file_to_sidebar(fname)
-
-        except Exception:
-            print('Error')
-
-    def save_file(self):
-        if self.filename == None:
-
-            # If no filename in state, which means new file.
-            f = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
-            if f is None:
-                return
-            text2save = str(self.text_area.get(1.0, END))
-            f.write(text2save)
-            f.close()
-        else:
-            # If filename in state, which means opened file
-            f = open(self.filename, "r+")
-            my_txt = str(self.text_area.get(1.0, END))
-            f.writelines(my_txt)
-            f.close()
-
-    def add_file_to_sidebar(self, name):
-        label = Label(self.sidebar, text=name, width=15, height=1, anchor=NW, justify=LEFT)
-        label.pack(fill=X)
+    def showFile(self, e):
+        print('clicked')
+        text1 = open(self.filename).read()
+        self.text_area.insert(END, text1)
 
 def pretty_filename(path):
     head, tail = ntpath.split(path)
